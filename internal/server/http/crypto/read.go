29e698a41750c1ea7ed509a38a092dc2952601dd
belliel <rkuserbaev@gmail.com>
2024-05-24 22:07:59 +0300

    feat(handler): implement bad request

--- internal/server/http/crypto/read.go
+++ internal/server/http/crypto/read.go
@@ -1,8 +1,10 @@
 package crypto
 
 import (
+	"errors"
 	"github.com/belliel/crypto-price-aggregator/internal/entities/crypto"
 	"github.com/belliel/crypto-price-aggregator/internal/server/http/api"
+	cryptoService "github.com/belliel/crypto-price-aggregator/internal/service/crypto"
 	"net/http"
 	"strings"
 )
@@ -12,6 +14,13 @@ func (h *Handler) LastTradedPrice(w http.ResponseWriter, r *http.Request) {
 
 	result, err := h.ServiceCrypto.GetLastTradedPrice(r.Context(), &crypto.GetLastTradedPriceQuery{Pairs: pairs})
 	if err != nil {
+		if errors.Is(err, cryptoService.ErrAPIHaveFailed) {
+			api.WriteError(w, http.StatusBadRequest, &api.Error{
+				Message: "bad request",
+				Code:    http.StatusBadRequest,
+			})
+			return
+		}
 		api.WriteError(w, http.StatusInternalServerError, &api.Error{
 			Message: "internal error",
 			Code:    http.StatusInternalServerError,
